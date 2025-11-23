2ebfdf0cdf72c6217770897f32ae6d6b357f0089
@erick <erickescribaa@gmail.com>
2025-11-23 01:23:17 -0500

    fix(router): repair routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,22 @@
-import { Route, Router, Switch } from "wouter";
-import { Step1 } from "./pages/SalesFlow";
+import { Redirect, Route, Router, Switch } from 'wouter';
+import { Header } from './common/components';
+import { ResumePage, Step1Page, Step2Page } from './pages/SalesFlow';
 
 export const Routes = () => (
-  <Router>
-    <Switch>
-      <Route path="/" component={Step1} />
-    </Switch>
-  </Router>
+  <>
+    <Router>
+      <Route path="/">
+        <Redirect to="/cotiza-tu-seguro-rimac" />
+      </Route>
+      <Header />
+      <Switch>
+        <Route path="/cotiza-tu-seguro-rimac" component={Step1Page} />
+        <Route path="/cotiza-tu-seguro-rimac/1" component={Step2Page} />
+        <Route path="/cotiza-tu-seguro-rimac/2" component={ResumePage} />
+        <Route path="/*">
+          <Redirect to="/cotiza-tu-seguro-rimac" />
+        </Route>
+      </Switch>
+    </Router>
+  </>
 );
